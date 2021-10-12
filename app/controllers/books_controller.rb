e3b477d447a1015a8c8f14d5116cc05ffdb75c61
kai690 <k.green0519.ap@icloud.com>
2021-10-12 09:14:36 +0000

    [add]index/show/edit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
+  end
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    blog = Book.find(params[:id])
+    blog.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
