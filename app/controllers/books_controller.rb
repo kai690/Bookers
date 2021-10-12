71e767b4fecfe194ce6e2cbf7a395678b452719f
kai690 <k.green0519.ap@icloud.com>
2021-10-12 13:54:52 +0000

    [add]update/destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,12 +19,20 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
   def update
-    blog = Book.find(params[:id])
-    blog.update(book_params)
-    redirect_to book_path(book)
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(book)
   end
-
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
